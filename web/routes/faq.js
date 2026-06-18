20b891b4351ed121c1ae6da788a634b6dd15fafd
supportsasatech-arch <supportsasatech@gmail.com>
2026-06-18 23:11:52 +0530

    SasaDev 🧏

---
+++ web/routes/faq.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import path from 'path';
+const router = express.Router();
+const __dirname = path.resolve();
+
+router.get('/faq', (req, res) => res.sendFile(path.join(__dirname, 'web', 'faq.html')));
+
+export default router;
